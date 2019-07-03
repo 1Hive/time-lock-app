a4dc8e7fc760e04afac0acc9509f9ab2c046c2af
William Griffiths <willjgriff@gmail.com>
2019-07-03 17:26:43 +0100

    Replaced mapping of lock times with an array to simplify. Prevents ability to add to the WithdrawLock struct in a contract upgrade but is much simpler.

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -3,18 +3,17 @@ pragma solidity ^0.4.24;
 import "@aragon/os/contracts/apps/AragonApp.sol";
 import "@aragon/os/contracts/common/IForwarder.sol";
 import "@aragon/os/contracts/lib/token/ERC20.sol";
-import "./lib/ArrayUtils.sol";
+import "./lib/WithdrawLockLib.sol";
 
 // TODO: Use SafeMath
 contract Lock is AragonApp, IForwarder {
 
-    using ArrayUtils for uint256;
+    using WithdrawLockLib for WithdrawLockLib.WithdrawLock[];
 
     bytes32 constant public CHANGE_DURATION_ROLE = keccak256("CHANGE_DURATION_ROLE");
     bytes32 constant public CHANGE_AMOUNT_ROLE = keccak256("CHANGE_AMOUNT_ROLE");
 
-    string private constant ERROR_TOO_MANY_UNLOCK_TIMES = "LOCK_TOO_MANY_UNLOCK_TIMES";
-    string private constant ERROR_WITHIN_LOCK_DURATION = "LOCK_WITHIN_LOCK_DURATION";
+    string private constant ERROR_TOO_MANY_WITHDRAW_LOCKS = "LOCK_TOO_MANY_WITHDRAW_LOCKS";
     string private constant ERROR_CAN_NOT_FORWARD = "VOTING_CAN_NOT_FORWARD";
 
     event ChangeLockDuration(uint256 newLockDuration);
@@ -24,23 +23,10 @@ contract Lock is AragonApp, IForwarder {
     uint256 lockDuration;
     uint256 lockAmount;
 
-    struct WithdrawLock {
-        uint256 unlockTime;
-        uint256 lockAmount;
-    }
-
-    // When needing an array of structs where the struct might be modified in a contract upgrade, it's better to use a
-    // mapping instead of an array. Updates to a struct referenced in an array could overwrite other structs in that
-    // array, where as updates to a struct referenced in a mapping would not overwrite other structs in that mapping.
-    // This is because an array's data is stored contiguously in storage, where as a mapping's data is stored sparsely
-    // in storage. In this case it means we can add to the WithdrawLock struct safely in a contract upgrade.
-    struct WithdrawLocks {
-        uint256 latestWithdrawnId;
-        uint256 newWithdrawLockId;
-        mapping(uint256 => WithdrawLock) withdrawLocks;
-    }
-
-    mapping(address => WithdrawLocks) addressesWithdrawLocks;
+    // Using an array instead of a mapping here means we cannot add fields to the WithdrawLock struct in
+    // an upgrade of this contract. If we want to be able to add to the WithdrawLock structure in future we must
+    // use a mapping instead.
+    mapping(address => WithdrawLockLib.WithdrawLock[]) addressesWithdrawLocks;
 
     function initialize(address _token, uint256 _lockDuration, uint256 _lockAmount) public onlyInit {
         token = ERC20(_token);
@@ -61,48 +47,25 @@ contract Lock is AragonApp, IForwarder {
     }
 
     function withdrawTokens() public {
-        WithdrawLocks storage addressWithdrawLocks = addressesWithdrawLocks[msg.sender];
-        uint256 maxNumberWithdrawLocks = addressWithdrawLocks.newWithdrawLockId - addressWithdrawLocks.latestWithdrawnId;
-        withdrawTokens(maxNumberWithdrawLocks);
+        WithdrawLockLib.WithdrawLock[] storage addressWithdrawLocks = addressesWithdrawLocks[msg.sender];
+        withdrawTokens(addressWithdrawLocks.length);
     }
 
-    // The approach used uses a mapping of id's (equivalent to array indices) to WithdrawLocks. New WithdrawLocks
-    // are added to the end of the mapping in forward(). The withdrawTokens() function iterates through the WithdrawLocks from
-    // latestWithdrawnId and saves the latestWithdrawnId, before a WithdrawLock that cannot be withdrawn. However, it continues
-    // iterating after a locked withdrawLock is found as the duration may have changed, meaning a withdrawLock later in
-    // the mapping could be withdrawable before the currently locked one in the iteration. The numberWithdrawLocks argument
-    // currently determines how many of the WithdrawLocks are searched, if there are many locked WithdrawLocks ahead of
-    // an unlocked WithdrawLock, numberWithdrawLocks may need to be increased to withdraw unlocked WithdrawLocks.
-    //
-    // A clearer approach would likely be to use a WithdrawLock[] array, where the WithdrawLock includes the owner of the lock
-    // and a mapping(address => uint256[]) representing mapping(userAddress => withdrawLockIds[]). We could copy the
-    // withdrawLockIds[] array, iterate through it and delete the withdrawn WithdrawLocks from the original as it goes.
     function withdrawTokens(uint256 numberWithdrawLocks) public {
-        WithdrawLocks storage addressWithdrawLocks = addressesWithdrawLocks[msg.sender];
+        WithdrawLockLib.WithdrawLock[] storage addressWithdrawLocksStorage = addressesWithdrawLocks[msg.sender];
+        WithdrawLockLib.WithdrawLock[] memory addressWithdrawLocksCopy = addressesWithdrawLocks[msg.sender];
 
-        require(addressWithdrawLocks.latestWithdrawnId + numberWithdrawLocks < addressWithdrawLocks.newWithdrawLockId,
-            ERROR_TOO_MANY_UNLOCK_TIMES);
+        require(numberWithdrawLocks < addressWithdrawLocksCopy.length, ERROR_TOO_MANY_WITHDRAW_LOCKS);
 
-        uint256 finalWithdrawLockId = addressWithdrawLocks.latestWithdrawnId + numberWithdrawLocks;
         uint256 amountOwed = 0;
-        uint256 newLatestWithdrawnId = addressWithdrawLocks.latestWithdrawnId;
-        bool lockedWithdrawLockFound = false;
 
-        for (uint256 withdrawLockId = addressWithdrawLocks.latestWithdrawnId;
-            withdrawLockId < finalWithdrawLockId; withdrawLockId++) {
+        for (uint256 withdrawLockIndex = 0; withdrawLockIndex < addressWithdrawLocksCopy.length; withdrawLockIndex++) {
 
-            WithdrawLock storage withdrawLock = addressWithdrawLocks.withdrawLocks[withdrawLockId];
+            WithdrawLockLib.WithdrawLock memory withdrawLock = addressWithdrawLocksCopy[withdrawLockIndex];
 
             if (now > withdrawLock.unlockTime) {
                 amountOwed += withdrawLock.lockAmount;
-                delete addressWithdrawLocks.withdrawLocks[withdrawLockId];
-
-                if (!lockedWithdrawLockFound) {
-                    newLatestWithdrawnId = withdrawLockId;
-                }
-
-            } else {
-                lockedWithdrawLockFound = true;
+                addressWithdrawLocksStorage.deleteItem(withdrawLock);
             }
         }
 
@@ -123,10 +86,8 @@ contract Lock is AragonApp, IForwarder {
     function forward(bytes evmCallScript) public {
         require(canForward(msg.sender, evmCallScript), ERROR_CAN_NOT_FORWARD);
 
-        WithdrawLocks storage addressWithdrawLocks = addressesWithdrawLocks[msg.sender];
-
-        addressWithdrawLocks.withdrawLocks[addressWithdrawLocks.newWithdrawLockId] = WithdrawLock(now + lockDuration, lockAmount);
-        addressWithdrawLocks.newWithdrawLockId++;
+        WithdrawLockLib.WithdrawLock[] storage addressWithdrawLocks = addressesWithdrawLocks[msg.sender];
+        addressWithdrawLocks.push(WithdrawLockLib.WithdrawLock(now + lockDuration, lockAmount));
 
         token.transferFrom(msg.sender, address(this), lockAmount);
 
--- contracts/lib/WithdrawLockLib.sol
+++ contracts/lib/WithdrawLockLib.sol
@@ -1,12 +1,17 @@
 pragma solidity ^0.4.24;
 
 
-library ArrayUtils {
+library WithdrawLockLib {
 
-   function deleteItem(uint256[] storage self, uint256 item) internal returns (bool) {
+    struct WithdrawLock {
+        uint256 unlockTime;
+        uint256 lockAmount;
+    }
+
+   function deleteItem(WithdrawLock[] storage self, WithdrawLock item) internal returns (bool) {
         uint256 length = self.length;
         for (uint256 i = 0; i < length; i++) {
-            if (self[i] == item) {
+            if (self[i].unlockTime == item.unlockTime && self[i].lockAmount == item.lockAmount) {
                 uint256 newLength = self.length - 1;
                 if (i != newLength) {
                     self[i] = self[newLength];
