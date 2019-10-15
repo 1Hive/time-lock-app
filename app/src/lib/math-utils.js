0f1a6f7092a876a99e98aa375c72ea1e4351a270
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2019-10-15 18:32:27 -0300

    Delete unused functions && add new empty screen

--- app/src/lib/math-utils.js
+++ app/src/lib/math-utils.js
@@ -14,9 +14,16 @@ export function formatDecimals(value, digits) {
   }
 }
 
-export function formatTokenAmount(amount, isIncoming, decimals = 0, displaySign = false, { rounding = 2 } = {}) {
+export function formatTokenAmount(
+  amount,
+  isIncoming,
+  decimals = 0,
+  displaySign = false,
+  { rounding = 2 } = {}
+) {
   return (
-    (displaySign ? (isIncoming ? '+' : '-') : '') + formatDecimals(round(amount / Math.pow(10, decimals), rounding), 18)
+    (displaySign ? (isIncoming ? '+' : '-') : '') +
+    formatDecimals(round(amount / Math.pow(10, decimals), rounding), 18)
   )
 }
 
@@ -36,85 +43,6 @@ export function round(num, places = 2) {
   return Number.isNaN(rounded) ? Number(num.toFixed(places)) : rounded
 }
 
-/**
- * Get the whole and decimal parts from a number.
- * Trims leading and trailing zeroes.
- *
- * @param {string} num the number
- * @returns {Array<string>} array with the [<whole>, <decimal>] parts of the number
- */
-function splitDecimalNumber(num) {
-  const [whole = '', dec = ''] = num.split('.')
-  return [
-    whole.replace(/^0*/, ''), // trim leading zeroes
-    dec.replace(/0*$/, ''), // trim trailing zeroes
-  ]
-}
-
-/**
- * Format a decimal-based number back to a normal number
- *
- * @param {string} num the number
- * @param {number} decimals number of decimal places
- * @param {Object} [options] options object
- * @param {bool} [options.truncate=true] Should the number be truncated to its decimal base
- * @returns {string} formatted number
- */
-export function fromDecimals(num, decimals, { truncate = true } = {}) {
-  const [whole, dec] = splitDecimalNumber(num)
-  if (!whole && !dec) {
-    return '0'
-  }
-
-  const paddedWhole = whole.padStart(decimals + 1, '0')
-  const decimalIndex = paddedWhole.length - decimals
-  const wholeWithoutBase = paddedWhole.slice(0, decimalIndex)
-  const decWithoutBase = paddedWhole.slice(decimalIndex)
-
-  if (!truncate && dec) {
-    // We need to keep all the zeroes in this case
-    return `${wholeWithoutBase}.${decWithoutBase}${dec}`
-  }
-
-  // Trim any trailing zeroes from the new decimals
-  const decWithoutBaseTrimmed = decWithoutBase.replace(/0*$/, '')
-  if (decWithoutBaseTrimmed) {
-    return `${wholeWithoutBase}.${decWithoutBaseTrimmed}`
-  }
-
-  return wholeWithoutBase
-}
-
-/**
- * Format the number to be in a given decimal base
- *
- * @param {string} num the number
- * @param {number} decimals number of decimal places
- * @param {Object} [options] options object
- * @param {bool} [options.truncate=true] Should the number be truncated to its decimal base
- * @returns {string} formatted number
- */
-export function toDecimals(num, decimals, { truncate = true } = {}) {
-  const [whole, dec] = splitDecimalNumber(num)
-  if (!whole && !dec) {
-    return '0'
-  }
-
-  const wholeLengthWithBase = whole.length + decimals
-  const withoutDecimals = (whole + dec).padEnd(wholeLengthWithBase, '0')
-  const wholeWithBase = withoutDecimals.slice(0, wholeLengthWithBase)
-
-  if (!truncate && wholeWithBase.length < withoutDecimals.length) {
-    return `${wholeWithBase}.${withoutDecimals.slice(wholeLengthWithBase)}`
-  }
-  return wholeWithBase
-}
-
-// Return 0 if denominator is 0 to avoid NaNs
-export function safeDiv(num, denom) {
-  return denom ? num / denom : 0
-}
-
 export function toHours(ms) {
   return ms / 3600000
 }
@@ -126,12 +54,17 @@ export function toHours(ms) {
  * @returns {string} formatted time
  */
 export function formatTime(time) {
+  const dayInSeconds = 86400
+  const hourInSeconds = 3600
+  const minuteInSeconds = 60
   const units = ['d', 'h', 'm', 's']
 
-  const days = Math.floor(time / 86400)
-  const hours = Math.floor((time % 86400) / 3600)
-  const minutes = Math.floor(((time % 86400) % 3600) / 60)
-  const seconds = ((time % 86400) % 3600) % 60
+  const days = Math.floor(time / dayInSeconds)
+  const hours = Math.floor((time % dayInSeconds) / hourInSeconds)
+  const minutes = Math.floor(((time % dayInSeconds) % hourInSeconds) / minuteInSeconds)
+  const seconds = ((time % dayInSeconds) % hourInSeconds) % minuteInSeconds
 
-  return [days, hours, minutes, seconds].map((elem, index) => (elem > 0 ? `${elem}${units[index]} ` : ''))
+  return [days, hours, minutes, seconds]
+    .map((elem, index) => (elem > 0 ? `${elem}${units[index]} ` : ''))
+    .join('')
 }
