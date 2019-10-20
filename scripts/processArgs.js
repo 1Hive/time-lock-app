8c674f41f09db2ea367964bcb6d50f4fd54f4db4
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2019-10-20 11:56:04 -0300

    Remove chalk dependency

--- scripts/processArgs.js
+++ scripts/processArgs.js
@@ -1,12 +1,12 @@
-//artifacts
+// artifacts
 const TokenBalanceOracle = this.artifacts.require('TokenBalanceOracle')
 
 const ANY_ENTITY = '0x'.padEnd(42, 'f')
 
-//role
+// role
 const ROLE = 'LOCK_TOKENS_ROLE'
 
-//params
+// params
 const ID = 'ORACLE_PARAM_ID'
 const OP = 'EQ'
 
@@ -14,14 +14,14 @@ module.exports = async () => {
   const args = process.argv.slice(4)
   const [daoAddress, timeLockAddress, oracleAddress] = args
 
-  //as the `dao acl grant` doesn't perform any check if the oracle address is in fact a contract, we perform a minimum check ourselves`
+  // as the `dao acl grant` doesn't perform any check if the oracle address is in fact a contract, we perform a minimum check ourselves`
   await checkOracle(oracleAddress)
   const param = [ID, OP, oracleAddress]
 
   console.log(daoAddress, timeLockAddress, ROLE, ANY_ENTITY, `"${param.join(',').replace(/' '/g, '')}"`)
 }
 
-/** Check if the address provided is a TokenBalanceOracle by calling one of it's getters */
+// Check if the address provided is a TokenBalanceOracle by calling one of it's getters
 const checkOracle = async address => {
   try {
     const oracle = await TokenBalanceOracle.at(address)
